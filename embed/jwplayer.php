07ad39ae879ee7de4f4f555063842d554bf43a4e
HyperXProject <HyperXProject@users.noreply.github.com>
2017-06-11 11:43:37 +0800

    Create jwplayer.php

--- embed/jwplayer.php
+++ embed/jwplayer.php
@@ -7,6 +7,7 @@
         <meta name="googlebot" content="noindex" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
         <script src="../dist/js/jquery.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="../dist/js/jwplayer.js"></script>
     </head>
 <body>
 <?php
